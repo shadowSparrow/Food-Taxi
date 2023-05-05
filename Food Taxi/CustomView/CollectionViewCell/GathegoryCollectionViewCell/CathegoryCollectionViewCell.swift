104f38b326a67ba8cf56ac3d65179cef1dfad1e4
shadowSparrow <romanenko.aleksandre@gmail.com>
2023-05-06 00:16:28 +0200

    NibFile for CathegoryCollectionViewCell

---
+++ Food Taxi/CustomView/CollectionViewCell/GathegoryCollectionViewCell/CathegoryCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CathegoryCollectionViewCell.swift
+//  Food Taxi
+//
+//  Created by mac on 06.05.2023.
+//
+
+import UIKit
+
+class CathegoryCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
